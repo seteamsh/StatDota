8430386fb5df5898f060186b77b8a4c20780e95f
seteamsh <seteamsh@gmail.com>
2025-11-23 20:17:50 +0500

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StatDota
+//
+//  Created by Temirlan Zhumashov on 23.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StatDotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatDotaApp.swift
+//  StatDota
+//
+//  Created by Temirlan Zhumashov on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct StatDotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
