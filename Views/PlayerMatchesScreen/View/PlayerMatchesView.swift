5529ca093758a3936c3e5e54a231f10da8248eb6
seteamsh <seteamsh@gmail.com>
2025-12-12 22:13:33 +0500

    before progress view data

---
+++ Views/PlayerMatchesScreen/View/PlayerMatchesView.swift
@@ -0,0 +1,22 @@
+//
+//  PlayerMatchesView.swift
+//  StatDota
+//
+//  Created by Temirlan Zhumashov on 11.12.2025.
+//
+
+import SwiftUI
+
+struct PlayerMatchesView: View {
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    PlayerMatchesView()
+}
